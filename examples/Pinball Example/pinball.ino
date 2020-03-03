631c746506c5d853b975ab72d187f81052ba5cc4
Jonathon Mascorella <joe_mascorella@me.com>
2020-03-03 15:31:32 +1100

    Fixed examples

--- examples/Pinball Example/pinball.ino
+++ examples/Pinball Example/pinball.ino
@@ -1,7 +1,4 @@
-#include <config_h.h>
-#include <constants.h>
 #include <TPIC_Shifter.h>
-
 
 // Add constants for the pins connected to the Arduino
 // from the pinball circuit
@@ -10,8 +7,10 @@ const int clockPin = 5;
 const int latchPin = 6;
 const int pinStatus = 7;
 const int ballReturnPin = 2;
+int max_lives = 3;
+int num_connected = 8;
 
-TPIC_Shifter pinballMachine = TPIC_Shifter(dataPin, clockPin, latchPin, pinStatus, ballReturnPin);
+TPIC_Shifter pinballMachine = TPIC_Shifter(dataPin, clockPin, latchPin, pinStatus, ballReturnPin, max_lives, num_connected);
 
 void setup() {
   // put your setup code here, to run once:
@@ -24,13 +23,13 @@ void loop() {
 
   // You may want to do something here to 
   if(pinballMachine.ballReturn() == true){
-    pinballMachine.gameLives(1);
+    pinballMachine.gameLives(0);
     pinballMachine.ledsFlash(10,20);
   }
 
-  if(pinballMachine.currentLives == 0){
-    delay(100);
-    pinballMachine.ledsDance(5,20);
+  if(pinballMachine.currentLives == 8){
+    delay(1000);
+    pinballMachine.ledsDance(10,30);
     pinballMachine.resetLives();
   }
   
